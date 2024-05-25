b01c5f8125de996894591fcf232f4ede1ad66ac6
Jhorman montoya <131841197+Jhorman2003@users.noreply.github.com>
2024-05-24 22:26:38 -0500

    Clientes VUE

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,56 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import EditarProducto from '../components/productos/EditarProducto.vue';
+import NewProducto from '../components/productos/NewProducto.vue';
+import Productos from '../views/Productos';
+
+import EditarVenta from '../components/ventas/EditarVenta.vue';
+import NewVenta from '../components/ventas/NewVenta.vue';
+import Ventas from '../views/Ventas';
+
+import Clientes from '../views/Clientes';
+
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/productos',
+    name: 'Productos',
+    component: Productos
+  },
+  {
+    path: '/add-producto/',
+    name: 'NewProducto',
+    component: NewProducto
+  },
+  {
+    path: '/editar-producto/:id',
+    name: 'EditarProducto',
+    component: EditarProducto
+  },
+  {
+    path: '/ventas',
+    name: 'Ventas',
+    component: Ventas
+  },
+  {
+    path: '/add-venta/',
+    name: 'NewVenta',
+    component: NewVenta
+  },
+  {
+    path: '/editar-venta/:id',
+    name: 'EditarVenta',
+    component: EditarVenta
+  },
+  {
+    path: '/clientes',
+    name: 'Clientes',
+    component: Clientes
   },
   {
     path: '/about',
